a0b9856b003c4f5c292eee5de5b0a182a6fe43ad
kalielze <kalielze@LAPTOP-2IH3HSPM.localdomain>
2023-07-02 00:23:06 +0700

    update module v2 parameter string func Materi

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kalielze/go-module
+module github.com/kalielze/go-module/v2
 
 go 1.18
--- sayhello.go
+++ sayhello.go
@@ -1,9 +1,11 @@
 package gomodule
 
+import "strconv"
+
 func SayHello() string {
 	return "Belajar module golang"
 }
 
-func Materi() string {
-	return "Module golang"
+func Materi(version int) string {
+	return "Module golang" + strconv.Itoa(version)
 }
